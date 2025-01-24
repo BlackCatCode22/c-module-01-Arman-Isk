805914881530ed78e8fafa0491fe1e9ec6d98262
Arman-Isk <iskenyanarman@gmail.com>
2025-01-23 22:21:10 -0800

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+//helloworld.cpp
+// Arman Iskenyan 1/23/2025
+#include <iostream>
+using namespace std;
+int sumTwoints(int num1, int num2) {
+    return num1 + num2;
+}
+int main() {
+    cout << "\n\n Hello, World! \n\n" << endl;
+
+    cout << sumTwoints(2, 3) << endl;
+
+    cout << "\n\n\n";
+    return 0;
+}
